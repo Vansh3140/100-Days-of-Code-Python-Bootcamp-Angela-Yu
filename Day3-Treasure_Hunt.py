5e1e1765bcea20b6b20158c9abb6daac6db2fc9f
Vansh3140 <95650499+Vansh3140@users.noreply.github.com>
2024-02-01 22:08:53 +0530

    Add files via upload

---
+++ Day3-Treasure_Hunt.py
@@ -0,0 +1,45 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+path=input("You are at the shipyard and there are 2 ships in front of you which ship do you want to board? Type 'left' or 'right'")
+
+if path=="right":
+  print("The Ship has sunk.....Game Overrrrrr!!!!")
+else:
+  dec1=input("There is a hole in your ship.Do you want to wait for help or swim through ocean? Type 'wait' or 'swim'.")
+  if dec1=="swim":
+    print("There are sharks in the ocean.....Game Overrrrrr!!!!")
+  else:
+    dec2=input("The rescue boat has taken you to an island where there are 3 boxes Red,Blue and Yellow.Choose one of 'red' , 'yellow' or 'blue'.")
+    if dec2=='red':
+      print("There were snakes in the box .......Game Overrrrrr!!!!!")
+    elif dec2=='yellow':
+      print("There was a poisonous gas in box.....Game Overrrrrr!!!!!")
+    else:
+      print("You Won the treasure Hurray!!!!")
+
