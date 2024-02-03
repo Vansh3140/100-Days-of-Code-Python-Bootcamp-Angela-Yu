71ff446a508fd0726bb7e8c2b5dbd5339d036078
Vansh3140 <95650499+Vansh3140@users.noreply.github.com>
2024-02-03 15:30:57 +0530

    Add files via upload

---
+++ Day5-Password_Generator.py
@@ -0,0 +1,40 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+password=""
+
+choice=input("Do you want an easy password or a hard one?Type 0 for easy and 1 for hard\n")
+
+for x in range(0,nr_letters):
+  num=random.randint(0,len(letters)-1)
+  password+=letters[num]
+
+for x in range(0,nr_symbols):
+  num=random.randint(0,len(symbols)-1)
+  password+=symbols[num]
+
+for x in range(0,nr_numbers):
+  num=random.randint(0,len(numbers)-1)
+  password+=numbers[num]
+
+if choice=="1":
+  list1=list(password)
+  random.shuffle(list1)
+  password=""
+  for x in list1:
+    password+=x
+
+print(f"Your password is {password}")
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
