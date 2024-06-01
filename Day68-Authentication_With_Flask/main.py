ff8b9571953dc18d91a0d3c3a1b8bb3304ccef19
Vansh3140 <95650499+Vansh3140@users.noreply.github.com>
2024-06-02 00:37:31 +0530

    Add files via upload

---
+++ Day68-Authentication_With_Flask/main.py
@@ -0,0 +1,111 @@
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+# CREATE TABLE IN DB
+class User(UserMixin, db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(1000))
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", logged_in=current_user.is_authenticated)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    if request.method == 'POST':
+        new_user = User(
+            email=request.form['email'],
+            password=generate_password_hash(request.form['password'], method='pbkdf2:sha256', salt_length=8),
+            name=request.form['name']
+        )
+        check_user = db.session.execute(db.select(User).where(User.email == new_user.email)).scalar()
+        if check_user:
+            flash('User already registered so login to move forward.', 'danger')
+            return redirect(url_for('login'))
+        else:
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect(url_for('secrets', name=new_user.name))
+    return render_template("register.html", logged_in=current_user.is_authenticated)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user_record = db.session.execute(db.select(User).where(User.email == email)).scalar()
+
+        if user_record and check_password_hash(user_record.password, password):
+            flash('Logged in successfully.', 'success')
+            login_user(user_record)
+            return redirect(url_for('secrets', name=user_record.name))
+        else:
+            if user_record:
+                flash('Login Unsuccessful. Please check your password', 'danger')
+            else:
+                flash('Login Unsuccessful. Please check your email', 'danger')
+
+    return render_template("login.html", logged_in=current_user.is_authenticated)
+
+
+@app.route('/secrets/<name>')
+@login_required
+def secrets(name):
+    return render_template("secrets.html", name=name, logged_in=True)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/download')
+@login_required
+def download():
+    return send_from_directory('static/files', 'cheat_sheet.pdf', as_attachment=True)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
